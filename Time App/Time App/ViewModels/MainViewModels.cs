cc696d820042e4af293a79c075fbec76829ab8a5
Pavol Hodás <pavolhodas4@gmail.com>
2021-01-15 16:15:15 +0100

    Add project files.

---
+++ Time App/Time App/ViewModels/MainViewModels.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Time_App.ViewModels
+{
+    
+    class MainViewModels : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModels()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+    }
+}
---
+++ Time App/Time App/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Time_App.ViewModels;
+using Xamarin.Forms;
+
+namespace Time_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModels();
+            
+        }
+    }
+}
