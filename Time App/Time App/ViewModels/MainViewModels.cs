d8bc318fd959361842de24917eeae19241edfb9b
Pavol Hodás <pavolhodas4@gmail.com>
2021-01-15 17:32:21 +0100

    Get function to write text when is button clicked

--- Time App/Time App/ViewModels/MainViewModels.cs
+++ Time App/Time App/ViewModels/MainViewModels.cs
@@ -1,29 +1,40 @@
-﻿using System.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Windows.Input;
+﻿using System;
+using System.ComponentModel;
 using Xamarin.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Time_App.ViewModels
 {
-    
+
     class MainViewModels : INotifyPropertyChanged
     {
         DateTime dateTime;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+
+        private string _labelText = "Hello";
+        public string LabelText
+        {
+            get => _labelText;
+            set
+            {
+                _labelText = value;
+                PropertyChanged?
+                .Invoke(this, new PropertyChangedEventArgs(nameof(LabelText)));
+            }
+        }
+        public Command ButtonClickedCommand => new Command(() =>
+        {
+            LabelText = "Miro Žbirka";
+        });
+
         public MainViewModels()
         {
-            this.DateTime = DateTime.Now;
+            DateTime = DateTime.Now;
 
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
-                this.DateTime = DateTime.Now;
+                DateTime = DateTime.Now;
                 return true;
             });
         }
