5a2c9dc1d8998310442138ac800631efc6159a0d
Pavol Hodás <pavolhodas4@gmail.com>
2021-01-26 19:21:53 +0100

    adjust app

--- Time App/Time App/ViewModels/TimerViewModels.cs
+++ Time App/Time App/ViewModels/TimerViewModels.cs
@@ -1,10 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
+using System.Timers;
+using Xamarin.Forms;
 
 namespace Time_App.ViewModels
 {
-    class TimerViewModels
+    public class TimerViewModels : INotifyPropertyChanged
     {
+        Stopwatch stopWatch = new Stopwatch();
+        private Timer time = new Timer();
+        private bool timerRunning;
+        private string _stopWatchHours;
+        public string StopWatchHours
+        {
+            get { return _stopWatchHours; }
+            set
+            {
+                _stopWatchHours = value;
+                OnPropertyChanged("StopvatchHours");
+            }
+        }
+
+        private string _stopWatchMinutes;
+        public string StopWatchMinutes
+        {
+            get { return _stopWatchMinutes; }
+            set
+            {
+                _stopWatchMinutes = value;
+                OnPropertyChanged("StopvatchMinutes");
+            }
+        }
+
+        private string _stopWatchSeconds;
+        public string StopWatchSeconds
+        {
+            get { return _stopWatchSeconds; }
+            set
+            {
+                _stopWatchSeconds = value;
+                OnPropertyChanged("StopvatchSeconds");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public TimerViewModels()
+        {
+            stopWatch.Start();
+            StopWatchHours = stopWatch.Elapsed.Hours.ToString();
+            StopWatchMinutes = stopWatch.Elapsed.Minutes.ToString();
+            StopWatchSeconds = stopWatch.Elapsed.Seconds.ToString();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                StopWatchHours = stopWatch.Elapsed.Hours.ToString();
+                StopWatchMinutes = stopWatch.Elapsed.Minutes.ToString();
+                StopWatchSeconds = stopWatch.Elapsed.Seconds.ToString();
+
+                return true;
+            });
+        }
     }
 }
--- Time App/Time App/Views/TimerPage.xaml.cs
+++ Time App/Time App/Views/TimerPage.xaml.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Time_App.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Time_App
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
     public partial class TimerPage : ContentPage
     {
         public TimerPage()
         {
             InitializeComponent();
+
+            BindingContext = new TimerViewModels();
         }
+
+        
     }
 }
